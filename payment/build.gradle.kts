1f8598002d3fe64f031fe188d745f9b07a40c2d4
Topy <gmsdl1994@naver.com>
2024-07-08 03:05:23 +0900

    build: 결제 서비스 kotlin-logging 추가

--- payment/build.gradle.kts
+++ payment/build.gradle.kts
@@ -1,48 +1,1 @@
-plugins {
-	id("org.springframework.boot") version "3.3.1"
-	id("io.spring.dependency-management") version "1.1.5"
-	kotlin("jvm") version "1.9.24"
-	kotlin("plugin.spring") version "1.9.24"
-}
-
-group = "topy"
-version = "0.0.1-SNAPSHOT"
-
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
-}
-
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
-}
-
-repositories {
-	mavenCentral()
-}
-
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.springframework.kafka:spring-kafka")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testImplementation("org.springframework.kafka:spring-kafka-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
-}
+plugins {	id("org.springframework.boot") version "3.3.1"	id("io.spring.dependency-management") version "1.1.5"	kotlin("jvm") version "1.9.24"	kotlin("plugin.spring") version "1.9.24"}group = "topy"version = "0.0.1-SNAPSHOT"java {	toolchain {		languageVersion = JavaLanguageVersion.of(21)	}}configurations {	compileOnly {		extendsFrom(configurations.annotationProcessor.get())	}}repositories {	mavenCentral()}dependencies {    implementation("org.springframework.boot:spring-boot-starter-web")    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")    implementation("org.jetbrains.kotlin:kotlin-reflect")    implementation("org.springframework.kafka:spring-kafka")    implementation("io.github.oshai:kotlin-logging:7.0.0")    compileOnly("org.projectlombok:lombok")    annotationProcessor("org.projectlombok:lombok")    testImplementation("org.springframework.boot:spring-boot-starter-test")    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")    testImplementation("io.mockk:mockk:1.13.11")    testImplementation("org.springframework.kafka:spring-kafka-test")    testRuntimeOnly("org.junit.platform:junit-platform-launcher")}kotlin {	compilerOptions {		freeCompilerArgs.addAll("-Xjsr305=strict")	}}tasks.withType<Test> {	useJUnitPlatform()}
