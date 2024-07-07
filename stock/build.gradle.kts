32330d9799a540153c7eb11df68f97589b3145b9
Topy <gmsdl1994@naver.com>
2024-07-08 00:48:19 +0900

    build: mockk 추가

--- stock/build.gradle.kts
+++ stock/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("io.mockk:mockk:1.13.11")
     testImplementation("org.springframework.kafka:spring-kafka-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
